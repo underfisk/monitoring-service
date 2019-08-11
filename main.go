9b18fd9bc0f987194c4fc9d0079681405f0136ed
Rodrigo <monstawoodwow@gmail.com>
2019-08-11 13:57:17 +0100

    Added frontend app

--- main.go
+++ main.go
@@ -7,11 +7,16 @@ import (
 	"github.com/labstack/echo/middleware"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
 	"time"
 	. "github.com/underfisk/monitorservice/src"
 	"github.com/labstack/echo"
+	"github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+	"github.com/joho/godotenv"
+	"os"
 )
 
 
@@ -21,19 +26,14 @@ var (
 	jwtSecret = "SOMETHING THAT WILL COME FROM PROCESS.ENV"
 )
 
-/**
-	This service collects metrics related with multiple servers/instances/apps
-	Also will be used to provide logs monitoring, service monitoring and frontend application
-	interface.
-	In order to manage the dashboard (visually) we'll be using Vue.js as a spa
-	and authorization based using JWT
-
-	Support:
-		- gRPC (For fast communication and create nestjs client for this)
-		- REST (http support)
- */
 func main () {
 	print("Running monitoring Service on port: 4000 for now")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	jwtSecret = os.Getenv("JWT_SECRET")
 
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 
@@ -48,27 +48,36 @@ func main () {
 	repo = NewRepository(client)
 	userRepo = NewUserRepository(client)
 
-/*	r := mux.NewRouter()
-	r.HandleFunc("/log", onLogPost).Methods("POST")
-	r.HandleFunc("/log", onLogsRetrieve).Methods("GET")
-*/
 
 	e := echo.New()
 
 	//Middlewares
 	e.Use(middleware.Recover())
+	e.Use(middleware.Gzip())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"https://localhost:4000"},
+		AllowHeaders: []string{ echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept },
+	}))
+
+	e.Use(middleware.BodyLimit("2M"))
+	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		TokenLookup: "header:X-XSRF-TOKEN",
+	}))
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status} latency=${latency_human} \n",
+	}))
+	//e.Use(middleware.Static("./public"))
 
 	//Routes
-	e.GET("/log", onLogsRetrieve)
-	e.POST("/login", login)
-
-	r := e.Group("/")
-	r.Use(middleware.JWT([]byte(jwtSecret)))
-	r.POST("/log", onLogPost)
+	e.GET("/logs", onLogsRetrieve, middleware.JWT([]byte(jwtSecret)))
+	e.POST("/login", onLogin)
+	e.POST("/register", onRegister)
+	e.POST("/log", onLogPost, middleware.JWT([]byte(jwtSecret)))
 
 
 	e.Logger.Fatal( e.Start(":4000"))
 }
+
 
 type LogDataDto struct {
 	Name string `json:name`
@@ -80,12 +89,109 @@ type LogDataDto struct {
 }
 
 
-func login(c echo.Context) error {
-	username := c.FormValue("username")
-	password := c.FormValue("password")
+type BadFieldErrorResponse struct {
+	Field string	`json:field`
+	Message string	`json:message`
+}
 
-	user,err := userRepo.GetByCredentials(username, password)
+type CreatedResponse struct {
+	Status string `json:status`
+}
+
+func onRegister (c echo.Context) error {
+	data := new(RegisterDto)
+	if err := c.Bind(data); err != nil {
+		return err
+	}
+
+	//Validate this data
+	log.Println(data)
+	if usernameErr := validation.Validate(
+		data.Username,
+		validation.Required,
+		validation.Length(3, 50),
+		is.Alphanumeric); usernameErr != nil {
+		res := &BadFieldErrorResponse {
+			Field: "username",
+			Message: usernameErr.Error(),
+		}
+		return c.JSON(400, res)
+	}
+
+
+	if pwdErr := validation.Validate(
+		data.Password,
+		validation.Required,
+		validation.Length(5, 255)); pwdErr != nil {
+		res := &BadFieldErrorResponse {
+			Field: "password",
+			Message: pwdErr.Error(),
+		}
+		return c.JSON(400, res)
+	}
+
+	if emailErr := validation.Validate(data.Email, validation.Required, is.Email); emailErr != nil {
+		res := &BadFieldErrorResponse {
+			Field: "email",
+			Message: emailErr.Error(),
+		}
+		return c.JSON(400, res)
+	}
+
+	if jobPosErr := validation.Validate(data.JobPosition, validation.Required, validation.In("Developer", "CTO", "CEO", "ETC")); jobPosErr != nil {
+		res := &BadFieldErrorResponse {
+			Field: "job_position",
+			Message: jobPosErr.Error(),
+		}
+		return c.JSON(400, res)
+	}
+
+	if companyErr := validation.Validate(data.Company, validation.Required, is.Alpha); companyErr != nil {
+		res := &BadFieldErrorResponse {
+			Field: "company",
+			Message: companyErr.Error(),
+		}
+		return c.JSON(400, res)
+	}
+
+	//If everything reaches here
+	log.Println("Data is okay lets register")
+
+	usrId, err := userRepo.Create(data)
+
 	if err != nil {
+		return err
+	}
+
+	res := &CreatedResponse{
+		Status: "ok",
+	}
+
+	log.Println("The id is: ", usrId)
+	return c.JSON(201, &res)
+}
+
+type LoginDto struct {
+	Email string `json:email`
+	Password string `json:password`
+}
+
+func onLogin(c echo.Context) error {
+	data := new(LoginDto)
+	if err := c.Bind(data); err != nil {
+		return err
+	}
+
+	log.Println("Logging email: ", data.Email)
+	user,err := userRepo.GetByEmail(data.Email)
+	if err != nil {
+		log.Println("Error getting user: ", err)
+		return echo.ErrUnauthorized
+	}
+
+	compareError := bcrypt.CompareHashAndPassword(user.Password, []byte(data.Password))
+	if compareError != nil {
+		log.Println("Invalid password", compareError)
 		return echo.ErrUnauthorized
 	}
 
@@ -94,7 +200,6 @@ func login(c echo.Context) error {
 
 	// Set claims
 	claims := token.Claims.(jwt.MapClaims)
-	claims["name"] = user.Name
 	claims["id"] = user.Id
 	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
 
--- src/user.repository.go
+++ src/user.repository.go
@@ -1,8 +1,13 @@
 package monitorservice
 
 import (
+	"context"
 	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+	"time"
 )
 
 type UserRepository struct {
@@ -10,13 +15,23 @@ type UserRepository struct {
 }
 
 type User struct {
-	Id uuid.UUID
-	Name string
+	Id string	`bson:"_id"`
+	Username string
+	Password []byte
 	Email string
 	CreatedAt string
 	JobPosition string /** what he does */
 	Company string
 	Avatar string
+}
+
+type RegisterDto struct {
+	Username string	`json:username`
+	Password string	`json:password`
+	Email string	`json:email`
+	JobPosition string	`json:jobPosition`
+	Company string	`json:company`
+
 }
 
 func NewUserRepository (mongo *mongo.Client) *UserRepository {
@@ -26,16 +41,60 @@ func NewUserRepository (mongo *mongo.Client) *UserRepository {
 }
 
 
-func (ur *UserRepository) Create () User {
-	return User{}
+func (ur *UserRepository) Create (data *RegisterDto) (uuid.UUID, error) {
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	collection := ur.mongo.Database("monitor_service").Collection("users")
+	id, _ := uuid.NewUUID()
+	pwd, hashErr := bcrypt.GenerateFromPassword( []byte(data.Password), bcrypt.DefaultCost)
+	if hashErr != nil {
+		return uuid.UUID{}, hashErr
+	}
+
+
+	_, err := collection.InsertOne(ctx, bson.M{
+		"_id":			 	id.String(),
+		"username":         data.Username,
+		"password": 		pwd,
+		"email":       	data.Email,
+		"job_position":         	data.JobPosition,
+		"avatar":	 	"",
+		"company":		data.Company,
+		"createdAt":	time.Now(),
+	})
+
+	if err != nil {
+		log.Println("Error: ", err)
+		return uuid.UUID{}, err
+	}
+
+	return id, nil
 }
 
 func (ur *UserRepository) GetById (id uuid.UUID) (User, error) {
 	return User{}, nil
 }
 
-func (ur *UserRepository) GetByCredentials (username string, password string) (User,error) {
-	return User{}, nil
+func (ur *UserRepository) GetByEmail(email string) (User, error) {
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	collection := ur.mongo.Database("monitor_service").Collection("users")
+
+	var usr User
+	err := collection.FindOne(ctx, bson.M{ "email": email }).Decode(&usr)
+
+	if err != nil {
+		return User{}, err
+	}
+
+	return User{
+		Id: usr.Id,
+		Username: usr.Username,
+		Password: usr.Password,
+		Company: usr.Company,
+		JobPosition: usr.JobPosition,
+		CreatedAt: usr.CreatedAt,
+		Avatar: usr.Avatar,
+		Email: usr.Email,
+	}, nil
 }
 
 func (ur *UserRepository) Update (id uuid.UUID, field string, value interface {}) {
